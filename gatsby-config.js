42e60b9d90670c556b405c5c6ddca4d97c6be3b7
Ratan Kaliani <ratankaliani@berkeley.edu>
2020-08-22 12:35:38 -0700

    update ref to icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -110,7 +110,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `/ratanlogo.png` // This path is relative to the root of the site.
+        icon: `static/ratanlogo.png` // This path is relative to the root of the site.
       }
     },
     "gatsby-plugin-sitemap",
